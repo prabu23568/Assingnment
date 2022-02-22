37ecf24c87b9197744ed3900c821184467a2411b
prabu23568 <84140715+prabu23568@users.noreply.github.com>
2022-02-22 10:57:32 +0530

    Add files via upload

---
+++ week1/java_lerning/paldrome.java
@@ -0,0 +1,24 @@
+package java_lerning;
+
+public class paldrome {
+	public static void main(String[] args) {
+		
+	
+	String input="madam";
+	String rev="";
+	System.out.println(input.length());
+	
+	for (int i = input.length()-1; i >=0; i--) {
+		
+		//System.out.println(input.charAt(i));
+		rev=rev+input.charAt(i);
+		//System.out.println(rev);
+	}
+	if(input.equals(rev)) {
+		System.out.println("palindrome");
+	}else {
+		System.out.println("not palindrome");
+
+}
+}
+}
